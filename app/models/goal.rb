f02df68ff5ff16d3b0b21ff2a1ab53c99702a9bf
Bianca Herbst <b.herbst@posteo.de>
2020-03-03 20:31:06 +0100

    add pg_search_scope to user

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,5 @@
 class Goal < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+  include PgSearch::Model
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  include PgSearch::Model
+  pg_search_scope :search_by_username_and_first_name_and_last_name,
+    against: [ :username, :first_name, :last_name ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
