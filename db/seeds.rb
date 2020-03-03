dd78bc1b3a97ed9f501705b17e8406e74ad640f5
Lino Maurer <lino@q-b.de>
2020-03-03 18:02:19 +0100

    minoe: added money rails gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # Display activity
+# puts 'Cleaning database'
+# # Destroying existing seeds
+# User.destroy_all
+# Activity.destroy_all
+# Goal.destroy_all
+# Milestone.destroy_all
+
+# # Display activity
+# puts 'Creating users'
+
+# 5.times do
+#   User.create(
+#     email: ,
+#     username: ,
+#     first_name: ,
+#     last_name: ,
+#     achievement_score: ,
+#     impressions: ,
+#     photo:
+#     )
+# end
+
+
+# # Display activity
+# puts 'Creating activities'
+
+# #
