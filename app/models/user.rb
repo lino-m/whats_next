a30530a256491a4c24ae3c54048e62495a5a7183
Bianca Herbst <b.herbst@posteo.de>
2020-03-04 11:31:44 +0100

    x

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   include PgSearch::Model
+  multisearchable against: [:username, :first_name, :last_name]
   pg_search_scope :search_by_username_and_first_name_and_last_name,
     against: => {
       :username => 'A',
       :last_name => 'B',
       :first_name => 'C'
-    }
+    },
     using: {
       tsearch: { prefix: true }
     }
