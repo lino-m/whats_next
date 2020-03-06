3d2987eb3c7c4bb3e9a52423fd04837debad6fc2
Bianca Herbst <b.herbst@posteo.de>
2020-03-06 12:48:16 +0100

    get display search working for either query present and in db and not present

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -6,36 +6,28 @@ before_action :find_goal, only: [:show]
 
   def new
     @goal = Goal.new
-    @goal.milestones.build
-    @activities = Activity.all
+    # @goal.build_milestones
   end
 
   def create
-    @goal = Goal.new(goal_params)
-    @goal.user = current_user
-    @activity = Activity.find(params[:goal][:activity][:name])
-    @goal.activity_id = @activity.id
-    if @goal.save!
-      redirect_to goals_path
-    else
-      render :new
-    end
   end
 
 
   def achievements
-    if params[:search].present?
+    if params[:search].present? #&& !params[:search][:query].match.nil?
       user_query = params[:search][:query]
-      # @achievements = Goal.where(completed: true)
-      @achievements_pg = PgSearch.multisearch(user_query)
-      @query_achievements = @achievements_pg.where(completed: true)
-
-    else
-
-      # @achievements = Goal.where(completed: true)
+      @goals_and_activities_pg = PgSearch.multisearch(user_query)
+      if @goals_and_activities_pg.empty?
+        @text = "Sorry, no matches. Look at what others did"
+        @achievements = Goal.where(completed: true)
+        @goals = @achievements.select { |a| a.class.name == 'Goal' }
+      else
+        @text = ''
+        @goals_and_activities = @goals_and_activities_pg.map(&:searchable)
+        @goals = @goals_and_activities.select { |goa| goa.class.name == 'Goal' }.select { |g| g.completed }
+        @activities = @goals_and_activities.select { |goa| goa.class.name == 'Activity'}
+      end
     end
-
-
   end
 
   def show
@@ -47,11 +39,27 @@ before_action :find_goal, only: [:show]
     @milestones = Milestone.where(goal_id: @achievement.id)
   end
 
-  # def notes to self
+  def searched
     # if params[:search].present? && params[:search][:query].match(/^\d+$/)
     #   user_input = params[:search][:query]
     #   @movies = Movie.where(year: user_input)
-  # end
+    # elsif params[:search].present?
+    #   user_input = params[:search][:query]
+    #   # @movies = Movie.where("title ILIKE ? OR syllabus ILIKE ? ", "%#{user_input}%", "%#{user_input}%" )
+    #   # sql_query = "
+    #   #   movies.title @@ :query
+    #   #   OR movies.syllabus @@ :query
+    #   #   OR directors.full_name @@ :query
+    #   # "
+    #   # @movies = Movie.joins(:director).where(sql_query, query: "%#{user_input}%")
+    #   # @movies = Movie.search_for_bananas(user_input)
+    #   @movies_pg_format = PgSearch.multisearch(user_input)
+    #   # @movies = @movies_pg_format.map { |movie| movie.searchable }
+    #   @movies = @movies_pg_format.map(&:searchable)
+    # else
+    #   @movies = Movie.all
+    # end
+  end
 
 
   private
@@ -59,20 +67,4 @@ before_action :find_goal, only: [:show]
   def find_goal
     @goal = Goal.find(params[:id])
   end
-
-  def goal_params
-    params.require(:goal).permit(:name, :description, :photo , milestones_attributes: [:name, :description, :done,  :price_cents]) #Milestone.attribute_names.map(:to_sym).push(:_destroy))
-  end
-
 end
-
-
-
-
-
-
-
-
-
-
-
