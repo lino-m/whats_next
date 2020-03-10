8fbdadb93725f76f2c5a7fcdf7648b0e2bee68e5
Lino Maurer <lino@q-b.de>
2020-03-10 19:31:46 +0100

    goal form: functionality established. Design: started but needs some love

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { hiddenSearch } from './hidden-search';
 import { initMapbox } from '../plugins/init_mapbox';
 import { searchCologne } from './search_buttons';
+import { totalSumGoalManager } from './total_sum_goal';
 // import { initAutocomplete } from '../plugins/init_mapbox';
 
 
@@ -14,5 +15,12 @@ searchCologne();
 
 initMapbox();
 
+totalSumGoalManager();
+
 // initAutocomplete(); #This musnt be commented in if it isnt finished.!!!!!
 
+
+const milestone_costs = document.querySelector('.milestone-price');
+
+milestone_costs.bootstrapValidator('enableFieldValidators',
+  'price_cents', false, 'notEmpty');
