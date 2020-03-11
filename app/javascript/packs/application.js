161bf84cec6bef4c005ed2cd29632464e10387a8
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-03-11 09:47:15 +0100

    no branch there

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { hiddenSearch } from './hidden-search';
 import { initMapbox } from '../plugins/init_mapbox';
-
+// another comment
 import { dropdownGoal } from './dropdown_goal';
 
 // import { initAutocomplete } from '../plugins/init_mapbox';
