f452b3206cb567162bf2a7fdf7f4c936af5bcc90
Bianca Herbst <b.herbst@posteo.de>
2020-03-11 18:42:44 +0100

    progressbar listern to checks

--- app/javascript/packs/progressbar.js
+++ app/javascript/packs/progressbar.js
@@ -1,28 +1,19 @@
 const progressbar = () => {
   const progressbars = document.getElementsByClassName("progress-bar");
   for(let progressbar of progressbars) {
-    const barId = progressbar.dataset.goalId
+    const barId = progressbar.dataset.goalId;
     const milestonesChecks = document.getElementsByClassName(`goal-card-${barId}`)
-    let progress = 0
+    let progress = 0;
     for(let check of milestonesChecks) {
       if (check.checked) {
         progress ++;
       }
     }
-    console.log(progress);
+    const updatedProgress = ((progress/milestonesChecks.length)* 100).toFixed(2) + '%';
+    console.log(updatedProgress);
+    const targetProgressbar = document.getElementById(`progbar-${barId}`);
+    targetProgressbar.style.width = updatedProgress;
   }
 }
-
-// <div class="btn btn-flat milestone_dropdown" data-card-id="<%= goal.id %>">
-//     see Milestones
-
-// after simpleforn<%= check_box_tag milestone.name, milestone.id, milestone.done , class: "goal-card-#{goal.id}"%>
-
-
-// <div class="progress-management">
-    // <div class="d-flex justify-content-between align-items-center">
-    //   <div class="progress" style="height: 9px;">
-// <div id="progbar-<%= goal.id %>" class="progress-bar" data-goal-id="<%= goal.id %>"  role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-
 
 export { progressbar };
