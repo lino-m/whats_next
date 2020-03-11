13f9c9c64d393b36c39fa08ce2dca632b08e587e
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-03-11 17:59:56 +0100

    pre mergin with master

---
+++ app/javascript/packs/ajax_milestones.js
@@ -0,0 +1,13 @@
+const checkboxMilestones = () => {
+  const checkboxes = document.getElementsByClassName('milestone-checkbox');
+  for (let checkbox of checkboxes) {
+    checkbox.addEventListener ('click', (event) => {
+     const targetId = event.currentTarget.dataset.milestoneId;
+     const targetSubmit = document.getElementById(`milestone-submit-${targetId}`)
+     targetSubmit.click()
+    });
+  }
+}
+
+
+export { checkboxMilestones };
