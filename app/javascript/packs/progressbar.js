7d29ee3317780220fb9a8e1431cedf3e7ac699b2
Bianca Herbst <b.herbst@posteo.de>
2020-03-12 10:26:41 +0100

    achieved-bar fixed

--- app/javascript/packs/progressbar.js
+++ app/javascript/packs/progressbar.js
@@ -1,17 +1,10 @@
 const progressbar = () => {
   const progressbars = document.getElementsByClassName("progress-bar");
   for(let progressbar of progressbars) {
-    const barId = progressbar.dataset.goalId;
-    const milestonesChecks = document.getElementsByClassName(`goal-card-${barId}`)
+    const goalId = progressbar.dataset.goalId;
+    const milestonesChecks = document.getElementsByClassName(`goal-card-${goalId}`)
     let progress = 0;
 
-    const achievedButton = document.getElementById("achieved-button");
-    achievedButton.addEventListener('click', () => {
-      targetProgressbar.style.width = 100 + '%';
-      for(let check of milestonesChecks) {
-      check.checked = true;
-      }
-    })
 
     for(let check of milestonesChecks) {
       if (check.checked) {
@@ -20,9 +13,20 @@ const progressbar = () => {
     }
     const updatedProgress = ((progress/milestonesChecks.length)* 100).toFixed(2) + '%';
     console.log(updatedProgress);
-    const targetProgressbar = document.getElementById(`progbar-${barId}`);
+    const targetProgressbar = document.getElementById(`progbar-${goalId}`);
     targetProgressbar.style.width = updatedProgress;
+
+    const achievedButton = document.getElementById(`achieved-button-${goalId}`);
+    achievedButton.addEventListener('click', () => {
+      targetProgressbar.style.width = 100 + '%';
+      for(let check of milestonesChecks) {
+      check.checked = true;
+      }
+    })
   }
 }
 
 export { progressbar };
+
+//
+
