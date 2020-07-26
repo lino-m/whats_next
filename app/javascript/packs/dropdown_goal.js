b268053a99298da5b93b587e92f1508c5cd5b5f0
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-07-26 14:47:50 +0200

    styled footer

--- app/javascript/packs/dropdown_goal.js
+++ app/javascript/packs/dropdown_goal.js
@@ -1,8 +1,7 @@
 const dropdownGoal = () => {
   const dropdownOpen = document.getElementsByClassName('milestone_dropdown');
-  console.log(dropdownOpen)
+
   for (let element of dropdownOpen) {
-    console.log(element)
     element.addEventListener('click', (event ) => {
       const targetId = event.currentTarget.dataset.cardId;
       const targetCard = document.getElementById(`card-${targetId}`)
